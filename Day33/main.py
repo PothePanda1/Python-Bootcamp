e92a5d5788864acfd9f508321e5ec64cd41f0c34
PothePanda1 <91084732+PothePanda1@users.noreply.github.com>
2025-06-25 01:09:59 +0100

    Update main.py

--- Day33/main.py
+++ Day33/main.py
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-import smtplib
-import datetime as dt
-import random
-
-my_email = "bijay765@gmail.com"
-password = "rbzhbtpoqldsjcuh"
-target_email = "765crimson@gmail.com"
-
-now = dt.datetime.now()
-day_of_the_week = now.weekday()
-
-if day_of_the_week == 3:
-
-    with open("quotes.txt") as f:
-        all_quotes = f.readlines()
-        quote = random.choice(all_quotes)
-        print(quote)
-
-    with smtplib.SMTP("smtp.gmail.com") as connection:
-        connection.starttls() #ENCRYPTS CONNECTION
-        connection.login(user=my_email, password= password)
-        connection.sendmail(
-            from_addr=my_email,
-            to_addrs=target_email,
-            msg=f"Subject:Monday Motivator\n\n{quote}"
-        )
-=======
 import requests
 from datetime import datetime
 import smtplib
